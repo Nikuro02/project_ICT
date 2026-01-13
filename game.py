b1b387a158d5c9d3384e7e8dc92e84b9003bc4ed
Nikuro02 <patipanlapoootama@gmail.com>
2026-01-13 21:16:58 +0700

    add game initialization and map files

--- game.py
+++ game.py
@@ -1,5 +1,15 @@
 import pygame, os, sys, random
 
-class Ghost:
-    def ghost(self):
-        self.ghost = pygame.image.load(os.path.join("",""))
+class GAME:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.font.init()
+
+        self.widht, self.height = 500, 700
+        self.screen = pygame.display.set_mode((self.widht, self.height))
+        pygame.display.set_caption("Flappy")
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
+        
+                
